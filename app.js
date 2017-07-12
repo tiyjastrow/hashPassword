3b867ad08ccc6398529f869327bc843e21ef2f0f
John Jastrow <jjastrow>
2017-07-12 14:58:43 -0500

    fixed the app.js sample code to match the updated isPasswordCorrect()

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var hashFunction = require("hash_function");
+var hashFunction = require("./hash_function");
 
 var newHash = hashFunction.hashPassword("chickenBones7!");
 console.log("newHash = ", newHash);
 
-var isValidated = hashFunction.isPasswordCorrect("chickenBones7!");
+var isValidated = hashFunction.isPasswordCorrect("chickenBones7!", newHash);
 console.log("Is it valid? ", isValidated);
